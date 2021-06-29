2de2f8b0602d3a237c5fd0e9103d7cf5846021b5
Yujin Sakuma <yujin12623@gmail.com>
2021-06-29 23:19:45 +0900

    Complete

--- svg_editor.php
+++ svg_editor.php
@@ -12,7 +12,7 @@ $tag_info = [ "rect", "circle", "ellipse", "line", "polyline", "polygon", "path"
  * @return 返還後のJSON
  */
 function svg_to_json( $fname ) {
-    $rdfile = './svg/' . $fname;
+    $rdfile = './original_svg/' . $fname;
     $svg = file_get_contents( $rdfile );
 
     $domDocument = new DOMDocument();
--- upload_svg.php
+++ upload_svg.php
@@ -4,10 +4,11 @@ $tempfile="";
 $filename="";
 if(isset($_FILES[ 'file' ])){
     $tempfile = $_FILES[ 'file' ][ 'tmp_name' ];
-    $filename = $_FILES[ 'file' ][ 'name' ];
+    $Originalfilename = $_FILES[ 'file' ][ 'name' ]; //
+    $filename=md5(uniqid(rand(),1)).".svg";
 }
 
-$furl = './svg/' . $filename;
+$furl = './original_svg/' . $filename;
 if ( is_uploaded_file( $tempfile ) ) {
     if ( move_uploaded_file( $tempfile, $furl ) ) {
         echo $filename . "をアップロードしました。";
@@ -18,24 +19,37 @@ if ( is_uploaded_file( $tempfile ) ) {
 
 function initialize_svgcode( $fname ) {
     $svg_json = svg_to_json( $fname );
+    write_file($fname,$svg_json,"Ojson");
+    
     $svg_array = json_to_array( $svg_json );
     $root = [ "ni" ];
     $tmpare = "svg";
     $id_seted_json = set_atribute_tag( "id", $tmpare, $svg_array );
-    $res_seted_json = set_atribute_tag( "res", $tmpare, $id_seted_json , "hello" );
+        
     
-    //echo( "<pre>" );
-    //var_dump( json_encode( $res_seted_json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES ) );
-    write_file(json_to_svgstring($res_seted_json),"svg");
-   var_dump(json_to_svgstring($res_seted_json));
-    //echo( "</pre>" );
-    
+    write_file($fname,json_to_svgstring($id_seted_json),"Esvg");
+    write_file($fname,json_encode($id_seted_json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES),"Ejson");
+   //echo($id_seted_json);
 }
 
-function write_file($writedata,string $type){
-    $fname=md5(uniqid(rand(),1));
-    $fpath="./edited_svg/";
-    file_put_contents($fpath.$fname.".svg", $writedata);
+function write_file($fname,$writedata,string $type){
+    
+    $ftype=["Esvg","Ejson","Ojson"];
+    $fpath=["./edited_svg/","./edited_svg_json/","./original_svg_json/"];
+    $fname = str_replace('.svg', '', str_replace('.json', '', $fname));
+    if($type=="Esvg"){
+        $fname.=".svg";
+    }elseif($type=="Ejson"||$type=="Ojson"){
+        $fname.=".json";
+    }
+    if($type=="Ejson"||$type=="Esvg"){
+        $fname="E_".$fname;
+    }
+    $p=array_search($type,$ftype);
+    
+    ini_set('display_errors', 0);
+    file_put_contents($fpath[$p].$fname, $writedata);
+    ini_set('display_errors', 1);
 }
 
 
