d824e5ee1763036be3fac202fbf24414b89529c7
Yujin Sakuma <yujin12623@gmail.com>
2021-07-01 13:07:01 +0900

    Prepare to make class object

--- json_edit.php
+++ json_edit.php
@@ -2,26 +2,53 @@
 class JSONEDIT{
     var $target_json;
     var $assosiative_array;
+    var $targe_attributes;    //参照渡し出来るか
+    var $target_attributes_key;
     
-    function jsonedit($name){
-        $url = "./edited_json/".$name;
+    function jsonedit($jname){
+        $url = "./edited_json/".$jname;
         $this->target_json = file_get_contents($url);
         $this->assosiative_array= json_decode( $svgjsondata, true );
+        
     }
-    
+    public function get_attributes($id){
+        //idに当たるアトリビュートキーデータをゲット（再起？）
+        //各Attributes編集保存時に毎回呼び出される
+        //返り値保存先は$target_attributes_key;
+        $this->targe_attributes = & $data[key];
+        $this->target_attributes_key = array_keys(& $data[key]);
+    }
+    public function tag_exist($tag){
+        //各Attributesにタグが存在するか確認
+        //各Attributes編集保存時、各詳細設定保存時に存在確認のため呼び出される
+    }
     
     
     
     function tag_edit(){
-        
-    }
-    function tag_exist(){
-        
-    }
-    function tag_add(){
-        
+        //再起
+        $targe_attributes= & $data[key];
+     
     }
     
+    
+    
+    
+    private function val_change(){
+        $target =  & $this->targe_attributes;
+       
+    }
+    private function tag_add(){
+        $target =  & $this->targe_attributes;
+    }
+    private function tag_del(){
+        $target =  & $this->targe_attributes;
+    }
+    
+    
+    public function close(){
+        
+    }
 }
 
 ?>
