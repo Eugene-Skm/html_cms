6c38b93182934d2f203c68ca737ab283777faff8
Yujin Sakuma <yujin12623@gmail.com>
2021-10-12 18:22:33 +0900

    add connect 2 start

--- class_db_io.php
+++ class_db_io.php
@@ -55,4 +55,49 @@ class connect {
     }
 }
 
+class connect2 {
+    //定数の宣言
+    const DB_NAME = 'svguser';
+    const HOST = 'localhost';
+    const UTF = 'utf8';
+    const USER = 'root';
+    const PASS = '';
+    //データベースに接続する関数
+    function pdo() {
+        $dsn = "mysql:dbname=" . self::DB_NAME . ";host=" . self::HOST . ";charset=" . self::UTF;
+        $user = self::USER;
+        $pass = self::PASS;
+        try {
+            $pdo = new PDO( $dsn, $user, $pass, array( PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES ' . SELF::UTF ) );
+        } catch ( Exception $e ) {
+            echo 'error' . $e->getMesseage;
+            die();
+        }
+        //エラーを表示してくれる。
+        $pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+        return $pdo;
+    }
+    //SELECT文用。
+    //汎用
+    function select( $sql ) {
+        $hoge = $this->pdo();
+        $stmt = $hoge->query( $sql );
+        $items = $stmt->fetchAll( PDO::FETCH_ASSOC );
+        return $items;
+    }
+    //特化
+    function select_listsvg() {
+        $sql="SELECT * FROM `svg_info`";
+        return $this->select($sql);        
+    }
+    //以下SELECT,INSERT,UPDATE,DELETE文
+    function inset_svg( $fname, $css, $js ) {
+        $sql = "INSERT INTO `svg_info`(`fname`, `css`, `js`) VALUES (:fname,:css,:js) ";
+        $hoge = $this->pdo();
+        $stmt = $hoge->prepare( $sql );
+        $stmt->execute( array( ':fname' => $fname, ':css' => $css, ':js' => $js ) );
+        return $stmt;
+    }
+}
+
 ?>
