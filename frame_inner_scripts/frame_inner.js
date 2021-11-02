fc533bb7d67cc0103267058f3ba5b6933d03c2db
Yujin Sakuma <yujin12623@gmail.com>
2021-11-03 00:01:46 +0900

    solved bubbling
    click eventlistner implements
    from child > parelent system implemented

--- frame_inner_scripts/frame_inner.js
+++ frame_inner_scripts/frame_inner.js
@@ -1,18 +1,49 @@
 // JavaScript Document
-setTimeout(function(){
-    var li = document.getElementsByTagName('dd');
-    console.log(li.length);
-    for (var i=0; i < li.length; i++) {
+setTimeout(function () { //iframe内 load待ち対策
 
-         li[i].addEventListener('click', function() {
-             
-            if(this.id==null||this.id==""){
-                this.id="temp_id";
+    //----------------------再起処理　ここから------------------------
+    //バブリング解決したから？？Z-indexいらんくね
+  /*  function strongLink(node, layer) {
+        if (node.nodeName === 'p' || node.nodeName === 'P') {
+            //特定要素に対する処理
+        }
+        try {
+            node.style.zIndex = layer;
+        } catch (e) {}
+        for (let i = 0; i < node.childNodes.length; i++) {
+            strongLink(node.childNodes[i], layer + 1); //再帰的呼び出し
+        }
+    }
+    //再起呼び出し　スタート地点　スクリプト言語のため呼び出し対象の後ろ側に来る
+    let contents = document.querySelector("html");
+    strongLink(contents, 0);*/
+    //----------------------再起処理　ここまで------------------------
+
+    var elementlist = document.querySelectorAll('*');
+    //elementlist.shift();
+    for (var i = 0; i < elementlist.length; i++) {
+
+        elementlist[i].addEventListener('click', function (event) {
+            //イベントバブリング　https:
+            //ja.javascript.info/bubbling-and-capturing
+            //イベントバブリング停止　
+            //https://itsakura.com/javascript-bubbling
+            event.stopPropagation();
+            console.log(event.target);
+            try {
+                //前回別カ所で作成されたtemp_idの削除
+                document.getElementById("temp_id").id = "";
+            } catch (e) {}
+
+            //idが無い場合、一時的に仮のidを作成
+            if (event.target.id == null || event.target.id == "") {
+                event.target.id = "temp_id";
             }
-            
-             var btn2=window.parent.document.getElementById("htmlt");
-             btn2.innerHTML = this.id;
-    },false);
+
+            //親iframe内要素に情報をセット
+            var titlecall = window.parent.document.getElementById("htmlt");
+            titlecall.innerHTML = event.target.tagName;
+        }, true);
     };
 
-},2000);
+}, 2000);
--- js/page_editor_jq.js
+++ js/page_editor_jq.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 $(function(){
     $('#htmlframe').on('load', function(){
-        $('#htmlframe').contents().find('head').append('<link rel="stylesheet" href="../frame_inner_scripts/frame_inner.css">');
-        $('#htmlframe').contents().find('head').append('<script type="text/javascript" src="../frame_inner_scripts/frame_inner.js"></script>');
+        $('#htmlframe').contents().find('head').append('<link id="temporary_css" rel="stylesheet" href="../frame_inner_scripts/frame_inner.css">');
+        $('#htmlframe').contents().find('head').append('<script　id="temporary_script" type="text/javascript" src="../frame_inner_scripts/frame_inner.js"></script>');
     });
 });
